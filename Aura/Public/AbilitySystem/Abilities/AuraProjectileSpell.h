0aea41f11bf6aa4a7696a1608f2cd2c298dea0a4
CzarekKol <cezary.kolpiewicz@onet.pl>
2025-08-12 16:09:27 +0200

    Fire Ball Projectile Ability

---
+++ Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/MyGameplayAbility.h"
+#include "AuraProjectileSpell.generated.h"
+
+class AAuraProjectile;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraProjectileSpell : public UMyGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<AAuraProjectile> ProjectileClass;
+	
+};
